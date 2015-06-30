d2bc5caf6235d7cef583091759b25e3a26afd681
sarahsga <sarahsga4@gmail.com>
2015-06-30 17:26:37 +0500

    starting with JSON

--- www/js/controllers/mainCtrl.js
+++ www/js/controllers/mainCtrl.js
@@ -1,5 +1,5 @@
 
-app.controller('MainCtrl',function($scope, $ionicSideMenuDelegate)
+app.controller('MainCtrl',function($scope, $ionicSideMenuDelegate, $ionicLoading, $http)
 {
     $scope.tabs_daily = true;
     $scope.four_star = true;
@@ -8,4 +8,29 @@ app.controller('MainCtrl',function($scope, $ionicSideMenuDelegate)
     $scope.toggleLeft = function() {
         $ionicSideMenuDelegate.toggleLeft();
     };
+
+    $scope.getJSON = function() {
+
+        $ionicLoading.show({
+            template: 'Loading...'
+        });
+
+        $http.get('http://cgtipster.com/api/InsertDataUsingSP.php').
+            success(function(data, status, headers, config) {
+                $ionicLoading.hide();
+                alert("success")
+                alert(JSON.stringify(data))
+                alert(JSON.stringify(status))
+                alert(JSON.stringify(headers))
+                alert(JSON.stringify(config))
+            }).
+            error(function(data, status, headers, config) {
+                $ionicLoading.hide();
+                alert("error")
+                alert(JSON.stringify(data))
+                alert(JSON.stringify(status))
+                alert(JSON.stringify(headers))
+                alert(JSON.stringify(config))
+            });
+    }
 });
